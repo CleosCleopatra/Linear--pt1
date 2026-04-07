83c9548876da10232bede413a16cb3c739acb882
Cleopatra <elsaerk09@gmail.com>
2026-04-07 17:14:37 +0200

    All

---
+++ Calculations.jl
@@ -0,0 +1,82 @@
+using JuMP      #load the package JuMP
+   #load the package Clp (an open linear-programming solver)
+using Gurobi   #The commercial optimizer Gurobi requires installation
+
+"""
+  Construct and returns the model of this assignment.
+"""
+include(parameter_value)
+
+function build_model(parameter_value::String)
+  # The diet problem
+  println("HUHUHUH")
+  crop2idx = Dict{String,Int64}()
+  fuel2idx = Dict{String,Int64}()
+  for i in I
+    crop2idx[crops[i]] = i
+  end
+  for j in J
+    fuel2idx[fuels[j]] = j
+  end
+  #I: set of  crops
+  #J: set of fuels
+  #crops: name of the foods, i in I
+  #fuels: name of the fuels, j in J
+
+  _y(i) = y[crop2idx[i]]
+  _w(i) = w[crop2idx[i]]
+  _o(i) = o[crop2idx[i]]
+
+  _f(j) = f[fuel2idx[j]]
+  _c(j) = c[fuel2idx[j]]
+  _t(j) = t[fuel2idx[j]]
+
+  _A_tot = A_tot 
+  _M_c = M_c
+  _P_c = P_c
+  _P_max = P_max
+  _W_max = W_max
+  _F_min = F_min
+
+  m = Model()
+
+  @variable(m, a[crops] >= 0) #Area for each crop
+  @constraint(m, sum(a[i] for i in crops) <= _A_tot)
+  @constraint(m, sum(a[i] * _w(i) for i in crops) <= _W_max)
+  V = sum(_y(i) * a[i] * _o(i) for i in crops)
+  B_tot = 0.9 * V
+  M = 0.2 * V
+  @variable(m, B[fuels] >= 0) #Amount of the differnt fuel types
+  @constraint(m, sum(B[j] * _f(j) for j in fuels)<= B_tot)
+  P = sum(B[j] * (1 - _f(j)) for j in fuels)
+  @constraint(m, P <= _P_max)
+
+  @objective(m, Max, sum(_c(j) * B[j] * (1-_t(j)) for j in fuels) - P*_P_c - M * _M_c)
+
+  @constraint(m, sum(B[j] for j in fuels) >= _F_min)
+
+  return m, a, B, P, M
+end
+
+
+#The ? can be put infront of commands, variables, and functions to get more information.
+#Note that is applied on the main object, hence to ask about an element in an array do:
+#element = array[5]
+#?element
+
+#Build the model and get variables and constraints back (see intro_mod.jl)
+include("parameter_values.jl")
+
+m, a, B, P, M = build_model("parameter_values.jl")
+print(m) # prints the model instance
+
+set_optimizer(m, Gurobi.Optimizer)
+set_optimizer_attribute(m, "OutputFlag", 1)
+set_optimizer(m, Clp.Optimizer)
+optimize!(m)
+
+println("Objective value =  ", objective_value(m))   		# display the optimal solution
+println("area for each crop =  ", value.(a.data))               # f.(arr) applies f to all elements of arr
+println("fuel amounts =  ", value.(B.data)) 
+println("petrol diesel amount =  ", value(P))
+println("Methanol amount =  ", value(M))    
---
+++ parameter_values.jl
@@ -0,0 +1,25 @@
+# Sets
+I = 1:3 # 3 different crops
+J = 1:3 # Set of different types of fuels
+
+#Labels
+crops = ["soybeans", "sunflower seeds", "cotton seeds"] # for i in I
+fuels  = ["b5", "b30", "b100"]
+
+#Parameters
+#N_min = [1800, 91, 60, 880]        #Minimum amount of nutrients [kcal, g, g, mg]
+#For crops
+y = [2600 1400 900] # Yield [t/ha]
+w = [5.0 4.2 1.0] #Water demand [Ml/ha]
+o = [0.178 0.216 0.433] #Oil content[l/kg]
+#For fuels
+f = [0.05 0.3 1.0] # Biodiesel percentage [%]
+c = [1.43 1.29 1.16] #Cost of biodiesel [pound/litre]
+t = [0.2 0.05 0] #Tax on fuel [percentage]
+
+A_tot = 1600 # Maximum area available [ha]
+M_c = 1.5 #Cost of methanol [pound/litre]
+P_c = 1 # Cost of petrol diesel [pound/litre]
+P_max = 150000 #Total amount of petrol diesel available [litre]
+W_max = 5000 #Total amount of water available [Ml]
+F_min = 280000 #minimum amount of fuel that needs to be delievered [litre]
