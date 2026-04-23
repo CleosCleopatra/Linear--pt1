a4361e9fb08f0412c44cce9de676a135cbe82db3
oskartormanen <oskar@tormanen.nu>
2026-04-23 14:35:58 +0200

    Oskar's code used to minimize constraints while staying feasible.

--- parameter_values.jl
+++ parameter_values.jl
@@ -9,7 +9,7 @@ fuels  = ["b5", "b30", "b100"]
 #Parameters
 #N_min = [1800, 91, 60, 880]        #Minimum amount of nutrients [kcal, g, g, mg]
 #For crops
-y = [2600 1400 900] # Yield [t/ha]
+y = [2600 1400 900] # Yield [kg/ha]
 w = [5.0 4.2 1.0] #Water demand [Ml/ha]
 o = [0.178 0.216 0.433] #Oil content[l/kg]
 #For fuels
@@ -19,7 +19,7 @@ t = [0.2 0.05 0] #Tax on fuel [percentage]
 
 A_tot = 1600 # Maximum area available [ha]
 M_c = 1.5 #Cost of methanol [pound/litre]
-P_c = 1 # Cost of petrol diesel [pound/litre]
-P_max = 150000 #Total amount of petrol diesel available [litre]
+D_c = 1 # Cost of petrol diesel [pound/litre]
+D_max = 150000 #Total amount of petrol diesel available [litre]
 W_max = 5000 #Total amount of water available [Ml]
 F_min = 280000 #minimum amount of fuel that needs to be delievered [litre]
